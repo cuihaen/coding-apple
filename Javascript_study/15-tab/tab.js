2758912805a9ce8c3e2b722d07872c3bd51e4098
cuiheen <choi_haeun367@naver.com>
2023-02-28 17:48:28 +0900

    tab 0 버튼 만들기

--- Javascript_study/15-tab/tab.js
+++ Javascript_study/15-tab/tab.js
@@ -9,3 +9,5 @@ document.querySelectorAll('.tab-button')[0].addEventListener('click',function(){
     document.querySelectorAll('.tab-content')[2].classList.remove('show');
     document.querySelectorAll('.tab-content')[0].classList.add('show');
 })
+
+//자주 쓰는 셀렉터 '변수'에 넣어서 사용하는 것을 권장!
