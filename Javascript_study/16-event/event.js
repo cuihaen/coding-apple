e974b94f6d67f66fb95e6ec189f4d659680d06b2
cuiheen <choi_haeun367@naver.com>
2023-03-01 23:39:32 +0900

    버그 수정

--- Javascript_study/16-event/event.js
+++ Javascript_study/16-event/event.js
@@ -20,8 +20,17 @@ for(let i = 0; i < document.querySelectorAll('.tab-button').length; i++){
 
 
 //까만 배경 눌렀을 때 modal창 닫는 기능 만들기!
-document.querySelector('.black-bg').addEventListener('click',function(){
-    document.querySelector('.black-bg').style.display='none';
+document.querySelector('.black-bg').addEventListener('click',function(e){
+    //e.target;  유저가 실제로 누른거 = 이벤트 발생한 곳.
+    //e.currentTarget;  이벤트 리스너 달린 곳 = this.
+    //e.preventDefault;  이벤트 기본 동작 막아줌 = ex)폼 전송을 막아준다.
+    //e.stopPropagation;  상위요소html로 퍼지는 이벤트를 막아준다.
+
+    //만약 내가 클릭한게 == 뒤에 나오는 요소와 같으면 해당 이벤트를 실행해라.
+    if(e.target == document.querySelector('.black-bg')){
+        document.querySelector('.black-bg').style.display='none';
+    }
+    
 })
 
-//이벤트 버블링
+//이벤트 버블링 = 이벤트가 상위 html로 퍼지는 현상
