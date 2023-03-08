011627bf99bdff60ce6d9b8fba4d99a875180f52
cuiheen <choi_haeun367@naver.com>
2023-03-08 14:45:41 +0900

    state학습

--- react_study/blog/src/App.js
+++ react_study/blog/src/App.js
@@ -1,16 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
 
   let post = '강남 우동 맛집';
+  let [글제목, b] = useState('남자코트추천');
+  //let num = [1,2];
+  //let a = num[0];
+  //let c = num[1];
+  //let [a,c] = [1,2]; Destructuring문법
   
   return (
     <div className="App">
       <div className="blackNav">
         <h1>HAEUN's Blog</h1>
       </div>
-      <h4>{ post }</h4>
+      <div className="list">
+        <h4>{ post }</h4>
+        <p>2023년 3월 8일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{ 글제목 }</h4>
+        <p>2023년 3월 8일 발행</p>
+      </div>
     </div>
   );
 }
