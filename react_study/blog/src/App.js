dac5e8591cd222ece4a154c922401da98a147951
cuiheen <choi_haeun367@naver.com>
2023-03-08 17:19:46 +0900

    좋아요 수 개별 변경

--- react_study/blog/src/App.js
+++ react_study/blog/src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   let post = '강남 우동 맛집';
   let [글제목, 글제목변경] = useState(['남자코트추천', '여자코트추천', 'react공부']);
-  let [ 따봉, 따봉변경 ] = useState(0);  //두번째 변수는 state변경용 함수임
+  let [ 따봉, 따봉변경 ] = useState([0,0,0]);  //두번째 변수는 state변경용 함수임
   let [ modal, setModal] = useState(false);
 
   [1,2,3].map(function(a){
@@ -26,11 +26,12 @@ function App() {
         copy.sort();
         글제목변경(copy);
       }}>글수정</button>
+
       {
         글제목.map(function(a,i){
           return (
-            <div className="list">
-              <h4>{ 글제목[i] }<span onClick = { ()=>{따봉변경(따봉+1)} } >👍🏻</span> { 따봉 } </h4>
+            <div className="list" key={i}>
+              <h4 onClick={()=>{setModal(!modal)}}>{ 글제목[i] }<span onClick = { ()=>{let copy = [...따봉]; copy[i] = copy[i]+1; 따봉변경(copy)} } >👍🏻</span> { 따봉[i] } </h4>
               <p>2023년 3월 8일 발행</p>
           </div>
           )
