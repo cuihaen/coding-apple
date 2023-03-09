a2b72c10d5660202bcafdef149f6ceeaa946e594
cuiheen <choi_haeun367@naver.com>
2023-03-09 18:25:58 +0900

    input 기능

--- react_study/blog/src/App.js
+++ react_study/blog/src/App.js
@@ -1,6 +1,4 @@
-/* eslint-disable */
 
-import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 
@@ -10,11 +8,9 @@ function App() {
   let [글제목, 글제목변경] = useState(['남자코트추천', '여자코트추천', 'react공부']);
   let [ 따봉, 따봉변경 ] = useState([0,0,0]);  //두번째 변수는 state변경용 함수임
   let [ modal, setModal] = useState(false);
+  let [ title, setTitle ] = useState(0);
+  let [ inputText, inputTextChange ]=useState(''); 
 
-  [1,2,3].map(function(a){
-    //array의 개수만큼 함수 안의 코드를 반복 실행해준다.
-    return '1233211'
-  })
   
   return (
     <div className="App">
@@ -31,16 +27,24 @@ function App() {
         글제목.map(function(a,i){
           return (
             <div className="list" key={i}>
-              <h4 onClick={()=>{setModal(!modal)}}>{ 글제목[i] }<span onClick = { ()=>{let copy = [...따봉]; copy[i] = copy[i]+1; 따봉변경(copy)} } >👍🏻</span> { 따봉[i] } </h4>
+              <h4 onClick={()=>{setModal(true); setTitle(i)}}>
+                { 글제목[i] }
+                <span onClick = {()=>{let copy = [...따봉]; copy[i] = copy[i]+1; 따봉변경(copy)} } >👍🏻</span> { 따봉[i] } </h4>
               <p>2023년 3월 8일 발행</p>
           </div>
           )
         })
       }
+      
+      <input onChange={(e)=>{inputTextChange(e.target.value);
+                            console.log(inputText)}}/>
 
       {
-        modal == true ? <Modal color="yellow" 글제목={글제목} /> : null
+        modal == true 
+        ? <Modal title = {title} 글제목={글제목}/> 
+        : null
       }
+
     </div>
   );
 }
@@ -49,10 +53,11 @@ function App() {
 function Modal(props){
   //함수명은 첫 글자는 반드시 대문자로 만들기!
   return(
-    <div className="modal" style={{background:props.color}}>
-      <h4>{props.글제목[0]}</h4>
+    <div className="modal">
+      <h4>{props.글제목[props.title]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button>글수정</button>
     </div>
   )
 }
