99bf7bb8e26e53ff3d466bd21998a42a45a210e0
cuiheen <choi_haeun367@naver.com>
2023-03-10 16:05:18 +0900

    img 추가

--- react_study/shop/src/App.js
+++ react_study/shop/src/App.js
@@ -1,23 +1,44 @@
 import './App.css';
-import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 
 function App() {
   return (
     <div className="App">
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">ShoeShop</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">Products</Nav.Link>
+            <Nav.Link href="#pricing">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
-      <Button variant="primary">Primary</Button>
+      <div className="mainBg"></div>
+      <Container>
+        <Row>
+          <Col>
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%" alt="신발1"/>
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </Col>
+          <Col>
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%" alt="신발2"/>
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </Col>
+          <Col>
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%" alt="신발3"/>
+            <h4>상품명</h4>
+            <p>상품정보</p>
+          </Col>
+        </Row>
+    </Container>
     </div>
   );
 }
