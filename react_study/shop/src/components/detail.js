36dc29f5d0c49569dc4e44ee287bb937e388ea73
cuiheen <choi_haeun367@naver.com>
2023-03-17 17:07:47 +0900

    useEffect clean up function 추가

--- react_study/shop/src/components/detail.js
+++ react_study/shop/src/components/detail.js
@@ -10,17 +10,25 @@ color:${props=>props.bg == 'blue' ? 'white' : 'black'};
 //만약 배경색이 blue라면 글자색을 하얀색으로, 아니면 블랙으로
 padding:10px;
 `
-//useEffect쓰는 이유 = 해당 영역 안에 있는 코드는 html렌더링 이후에 동작하기 때문.
+//useEffect쓰는 이유 = 해당 영역 안에 있는 코드는 html렌더링 이후에 동작하기 때문. = side effect 보관함.
 //useEffect는 일반적으로 어려운 연산, 서버에서 데이터 가져오는 작업, 타이머 장착하는 코드 들을 안에 많이 적는다.
 
 function Detail(props){
 
-    useEffect(()=>{
-        console.log('안녕')
-    })
-
+    let [alert,setAlert] = useState(true);
     let [count,setCount] = useState(0);
 
+    useEffect(()=>{
+        let a = setTimeout(()=>{
+            setAlert(false)
+        },2000)
+        return()=>{
+            //useEffect동작 전에 실행되는 return()=>{} < clean up function이라고 불림!
+            //clean up function은 mount시 실행 안됨, unmount(컴포넌트 삭제)시에는 실행됨.
+            clearTimeout(a); //기존 타이머는 제거해주세요. = clearTimeout 타이머 제거 함수.
+        }
+    },[])
+    //useEffect실행조건 넣을 수 있는 곳 [] state나 변수를 넣을 수 있다.
 
    let {id} = useParams(); //현재 URL의 파라미터!
    let findProduct = props.shoes.find(function(x){
@@ -28,6 +36,12 @@ function Detail(props){
    });
     return(
         <Container>
+            {
+            alert ==true ?
+                <div className="alert alert-warning">2초이내 구매시 할인</div> 
+            : null
+            }
+            
             {count}
             <button onClick={()=>{setCount(count+1)}}>버튼</button>
             <YellowBtn bg="blue">버튼</YellowBtn>
